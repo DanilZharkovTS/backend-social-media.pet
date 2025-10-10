617577cf675358432afe66807473e0d57a27e0e4
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-10-10 00:00:27 -0600

    feature/middleware-json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,33 @@
 import express, { Router } from 'express'
-import type { Request, Response } from 'express'
-import pool from './pool.ts'
+import type { NextFunction, Request, Response } from 'express'
 import postRoutes from './routes/postRoutes.ts'
 import { errHandler } from './middlewares/errHandler.ts'
+import { jsonMiddleware } from './middlewares/jsonMiddleware.ts'
+
 const app = express()
 const PORT = 3000
 const router = Router()
 
-app.use(express.json())
+app.use(
+  express.json({
+    verify: (req: Request, res: Response, buf: Buffer) => {
+      try {
+        JSON.parse(buf.toString())
+      } catch (err) {
+        throw new SyntaxError('Invalid JSON')
+      }
+    },
+  })
+)
+
+app.use(jsonMiddleware)
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof SyntaxError && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid JSON format' })
+  }
+  next(err)
+})
 
 app.use('/api/post', postRoutes)
 
