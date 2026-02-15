c38c8538738ea7575d5ef6919a12e5664a4d860d
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-02-15 23:04:19 +0000

    chore: add post test

--- src/services/user/postService.test.ts
+++ src/services/user/postService.test.ts
@@ -1,1 +1,23 @@
+import { postRepo } from '../../repos/postRepo'
+import { postService } from './postService'
 
+jest.mock('../../repos/postRepo')
+
+describe('postService', () => {
+  describe('addPost', () => {
+    const mockData = { description: 'Hello this is test' }
+    const mockUser = { userId: 15, email: 'adminka@gmail.com', role: 'admin' }
+    test('returns created post', async () => {
+      (postRepo.insert as jest.Mock).mockResolvedValue({
+        rows: [{ id: 123, user_id: 15, description: 'Hello this is test' }],
+      })
+
+      const result = await postService.add(mockData, mockUser)
+
+      expect(postRepo.insert).toHaveBeenCalledWith(15, 'Hello this is test')
+      expect(result).toStrictEqual({
+        created: { id: 123, user_id: 15, description: 'Hello this is test' },
+      })
+    })
+  })
+})
