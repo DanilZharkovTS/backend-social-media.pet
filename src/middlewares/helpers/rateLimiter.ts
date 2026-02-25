7057907c27a3d909195b54a2edffaf7de59f2d06
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-02-25 20:21:47 +0000

    rate limiting middleware

---
+++ src/middlewares/helpers/rateLimiter.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from 'express'
+import { getRedis } from '../../lib/redisClient'
+import { ApiError } from '../../lib/ApiErrors'
+
+export const rateLimiter = (
+  limit: number,
+  window: number,
+  endpoint: string
+) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const redis = getRedis()
+      const ip = req.ip
+      const key = `rate:${endpoint}:${ip}`
+
+      const requests = await redis.incr(key)
+
+      if (requests === 1) {
+        await redis.expire(key, window)
+      }
+
+      if (requests > limit) {
+        throw ApiError('Too many requests', 429)
+      }
+
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+}
