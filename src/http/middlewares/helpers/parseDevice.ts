6d436256a89ee01d9c9eab58b57f76386f5011cf
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-06-10 12:57:11 +0100

    initial setup of files and packages

---
+++ src/http/middlewares/helpers/parseDevice.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from "express"
+import { UAParser } from "ua-parser-js"
+
+export const parseDevice = (req: Request, res: Response, next: NextFunction) => {
+  
+}
