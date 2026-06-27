b9575167fcba190380f1bb616cc6f0f1bcc0bdb8
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-06-27 17:55:26 +0300

    feat: io rate limiter middleware

--- src/websocket/middlewares/helpers/rateLimiter.ts
+++ src/websocket/middlewares/helpers/rateLimiter.ts
@@ -1,4 +1,32 @@
+import { Socket } from 'socket.io'
+import { IoNextFn } from '../../../shared/interfaces/global/socket'
+import { getRedis } from '../../../shared/lib/redisClient'
+import { ApiError } from '../../../shared/lib/ApiErrors'
 
-export const ioRateLimit = () => {
-  
+export const ioRateLimiter = (
+  limit: number,
+  window: number,
+  endpoint: string
+) => {
+  return async (socket: Socket, data: any, ctx: any, next: IoNextFn) => {
+    try {
+      const redis = getRedis()
+      const ip = socket.handshake.address
+      const key = `rate:${endpoint}:${ip}`
+
+      const requests = await redis.incr(key)
+
+      if (requests === 1) {
+        await redis.expire(key, window)
+      }
+
+      if (requests > limit) {
+        throw ApiError('Too many requests', 429)
+      }
+
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
 }
