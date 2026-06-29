5b2edeb7683086b57e4127b41cf89b592138550f
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-06-29 23:42:36 +0300

    refactor: open ai provider

--- src/shared/services/user/ai/aiProvider.ts
+++ src/shared/services/user/ai/aiProvider.ts
@@ -1,50 +1,19 @@
 import OpenAI from 'openai'
+import { openAiProvider } from './openAiProvider'
+
+const generateRepliesHandler = {
+  openAi: openAiProvider.generateReplies,
+}
 
 export const aiProvider = {
   generateReplies: async (peeps: string[], count: number) => {
-    console.log(peeps)
+    const provider = process.env.AI_PROVIDER
+    const handler = generateRepliesHandler[provider]
 
-    const client = new OpenAI({
-      baseURL: process.env.AI_BASE_URL,
-      apiKey: process.env.AI_API_KEY,
-    })
-    const res = await client.responses.create({
-      model: process.env.AI_MODEL,
-      input: `
-      You are an AI assistant for a messenger.
+    if (!handler) return
 
-      You are NOT talking to the user.
+    const replies = await handler(peeps, count)
 
-      Your task is to generate reply suggestions that the sender could send to another person in a private chat.
-
-      If the last message asks about personal activities, events, experiences, or opinions (e.g. "What are you doing?", "How did your meeting go yesterday?"), generate generic, natural replies that do not invent specific facts unless they are provided in the conversation context.
-
-      Be friendly and like real person
-
-      The other person wrote:
-
-      "${peeps.join('\n')}"
-
-      Generate exactly ${count} natural, short replies.
-
-      Rules:
-      - Return only valid JSON.
-      - English
-      - No markdown.
-      - No explanations.
-      - Be very fast 
-      - The format must be:
-
-      {
-        "replies": [
-          "reply 1",
-          "reply 2",
-          "reply 3"
-        ]
-      }
-      `,
-    })
-
-    return JSON.parse(res.output_text)
+    return replies
   },
 }
